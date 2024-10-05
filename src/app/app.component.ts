a33d87a605a112e8915b0101b77165c5e3e93c0e
Mark Mulli <markmulli001@gmail.com>
2024-10-05 11:55:03 +0300

    structuring components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { HeaderComponent } from "./header/header.component";
 import { ServerDashboardComponent } from "./dashboard/server-dashboard/server-dashboard.component";
 import { TrafficDashboardComponent } from "./dashboard/traffic-dashboard/traffic-dashboard.component";
 import { SticketDashboardComponent } from "./dashboard/sticket-dashboard/sticket-dashboard.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerDashboardComponent, TrafficDashboardComponent, SticketDashboardComponent],
+  imports: [HeaderComponent, ServerDashboardComponent, TrafficDashboardComponent, SticketDashboardComponent, DashboardItemComponent],
 })
 export class AppComponent {
 
