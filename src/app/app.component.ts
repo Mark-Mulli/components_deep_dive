4a5c27791a5efb5f1b20032a59a5d2cd213e088d
Mark Mulli <markmulli001@gmail.com>
2024-10-05 11:04:44 +0300

    splitting into multiple components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,16 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { ServerDashboardComponent } from "./dashboard/server-dashboard/server-dashboard.component";
+import { TrafficDashboardComponent } from "./dashboard/traffic-dashboard/traffic-dashboard.component";
+import { SticketDashboardComponent } from "./dashboard/sticket-dashboard/sticket-dashboard.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [HeaderComponent, ServerDashboardComponent, TrafficDashboardComponent, SticketDashboardComponent],
 })
 export class AppComponent {
-  dummyTrafficData = [
-    {
-      id: 'd1',
-      value: 433,
-    },
-    {
-      id: 'd2',
-      value: 260,
-    },
-    {
-      id: 'd3',
-      value: 290,
-    },
-    {
-      id: 'd4',
-      value: 410,
-    },
-    {
-      id: 'd5',
-      value: 397,
-    },
-    {
-      id: 'd6',
-      value: 488,
-    },
-    {
-      id: 'd47',
-      value: 589,
-    },
-  ];
-  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
-  currentStatus = 'online';
+
+
 }
---
+++ src/app/dashboard/sticket-dashboard/sticket-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sticket-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './sticket-dashboard.component.html',
+  styleUrl: './sticket-dashboard.component.css'
+})
+export class SticketDashboardComponent {
+
+}
