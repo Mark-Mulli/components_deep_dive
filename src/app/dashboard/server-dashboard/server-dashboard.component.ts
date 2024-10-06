f1af553ab2e315843d20296656199a1fb3b26719
Mark Mulli <markmulli001@gmail.com>
2024-10-06 08:32:34 +0300

    component lifecycle

--- src/app/dashboard/server-dashboard/server-dashboard.component.ts
+++ src/app/dashboard/server-dashboard/server-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-dashboard',
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './server-dashboard.component.html',
   styleUrl: './server-dashboard.component.css'
 })
-export class ServerDashboardComponent {
-  currentStatus = 'online';
+export class ServerDashboardComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  constructor() {}
+
+  ngOnInit() {
+    setInterval(() => {
+
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown'
+      }
+
+    }, 3000)
+  }
 }
